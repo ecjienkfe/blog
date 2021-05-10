a598c04a193456d5ff0adb6e64ea965b2ae74f3c
ecjienkfe <moub545845@163.com>
2021-05-10 18:54:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 var http=require("http")
 //var url=require("url")
 
-function strat(){
+function strat(route,rrr){
     function onRequest(request,response){
-        //var pathname=url.parse(request.url).pathname
+        var pathname=url.parse(request.url).pathname
         response.wrriteHead(200,{"Content-Type":"text/plain"})
         response.write("yyyyyy")
         response.end()
         //var postData="sfggggffg"
         //route(rrr,pathname,response,postData)
     }
-http.createServer(onRquest).listen(8888)
+http.createServer(onRequest).listen(8888)
 }
 
 exports.start=start

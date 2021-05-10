8c2e3274f2e62d93607600e684eca8fdaee3fca8
ecjienkfe <moub545845@163.com>
2021-05-10 18:02:40 +0800

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,15 @@
 //路由
+
+
+function route(rrr,pathname,response,postData){
+    if(typeof rrr[pathname]==='function')
+    {
+        rrr[pathname](response,postData)
+    }
+    else
+    {
+        response.writeHead(404,{"Content-Type":"text/plain"})
+        response.write("404 Not Found")
+        response.end()
+    }
+}

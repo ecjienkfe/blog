724679e3cda39e4b79caf346b08f9217ddd7f902
ecjienkfe <moub545845@163.com>
2021-05-10 18:57:25 +0800

    Update rrr.js

--- rrr.js
+++ rrr.js
@@ -1,9 +1,9 @@
 //路径对应事件
 
-function start(response,postData){
+function wel(response,postData){
     response.writeHead(200,{'Content-Type':'text/html'})
     response.write('hello start')
     response.end()
 }
 
-exports.start=start
+exports.wel=wel

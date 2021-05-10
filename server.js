def117d6cc46bc1c8d511f4f63b46420a9611a51
ecjienkfe <moub545845@163.com>
2021-05-10 18:58:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ var url=require("url")
 function strat(route,rrr){
     function onRequest(request,response){
         var pathname=url.parse(request.url).pathname
-        response.wrriteHead(200,{"Content-Type":"text/plain"})
-        response.write("yyyyyy")
-        response.end()
-        //var postData="sfggggffg"
-        //route(rrr,pathname,response,postData)
+        var postData="sfggggffg"
+        route(rrr,pathname,response,postData)
     }
 http.createServer(onRequest).listen(8888)
 }
